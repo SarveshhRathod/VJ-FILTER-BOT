6d620812a1101803fcf656aaa0ca8116dd386481
Sarvesh Asatkar <sarveshh.asatkarr@gmail.com>
2026-09-04 15:02:06 +0530

    Update files_delete.py

--- plugins/files_delete.py
+++ plugins/files_delete.py
@@ -5,15 +5,14 @@
 import re, logging
 from pyrogram import Client, filters
 from info import DELETE_CHANNELS
-from database.ia_filterdb import col, sec_col, unpack_new_file_id
+from database.ia_filterdb import col, sec_col, unpack_new_file_id, MULTIPLE_DATABASE
 
 logger = logging.getLogger(__name__)
 media_filter = filters.document | filters.video
 
 @Client.on_message(filters.chat(DELETE_CHANNELS) & media_filter)
 async def deletemultiplemedia(bot, message):
-    """Delete Multiple files from database"""
-
+    """Delete Multiple files from database asynchronously"""
     for file_type in ("document", "video", "audio"):
         media = getattr(message, file_type, None)
         if media is not None:
@@ -21,15 +20,12 @@ async def deletemultiplemedia(bot, message):
     else:
         return
 
-    file_id, file_ref = unpack_new_file_id(media.file_id)
+    file_id = unpack_new_file_id(media.file_id)
 
-    result = col.delete_one({
-        'file_id': file_id,
-    })
-    if not result.deleted_count:
-        result = sec_col.delete_one({
-            'file_id': file_id,
-        })
+    result = await col.delete_one({'file_id': file_id})
+    if not result.deleted_count and MULTIPLE_DATABASE and sec_col is not None:
+        result = await sec_col.delete_one({'file_id': file_id})
+
     if result.deleted_count:
         logger.info('File is successfully deleted from database.')
     else:
@@ -39,29 +35,16 @@ async def deletemultiplemedia(bot, message):
             file_name = file_name.replace(char, '')
         file_name = ' '.join(filter(lambda x: not x.startswith('@'), file_name.split()))
     
-        result = col.delete_many({
+        result = await col.delete_many({
             'file_name': file_name,
             'file_size': media.file_size
         })
-        if not result.deleted_count:
-            result = sec_col.delete_many({
+        if not result.deleted_count and MULTIPLE_DATABASE and sec_col is not None:
+            result = await sec_col.delete_many({
                 'file_name': file_name,
                 'file_size': media.file_size
             })
         if result.deleted_count:
             logger.info('File is successfully deleted from database.')
         else:
-            result = col.delete_many({
-                'file_name': media.file_name,
-                'file_size': media.file_size
-            })
-            if not result.deleted_count:
-                result = sec_col.delete_many({
-                    'file_name': media.file_name,
-                    'file_size': media.file_size
-                })
-            if result.deleted_count:
-                logger.info('File is successfully deleted from database.')
-            else:
-                logger.info('File not found in database.')
-
+            logger.info('File not found in database.')
